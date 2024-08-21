dfeb606db64db79d81bc035033bfa4a11b49e122
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-08-21 15:03:24 -0300

    Criar injecter

---
+++ API/Injecter/ConfigInjecter.cs
@@ -0,0 +1,32 @@
+﻿using Server.BusinessRules;
+using System.Provider;
+
+namespace API.Injecter
+{
+    public static class ConfigInjecter
+    {
+
+        public static void Inject(IServiceCollection Services)
+        {
+            InjecterDefault(Services);
+            InjecterBusinessRules(Services);
+            InjecterProvider(Services);
+        }
+
+        public static void InjecterDefault(IServiceCollection Services)
+        {
+            Services.AddScoped<ApplicationDbContext>();
+        }
+
+        public static void InjecterBusinessRules(IServiceCollection Services)
+        {
+            Services.AddScoped<LogBusinessRules>();
+
+        }
+
+        public static void InjecterProvider(IServiceCollection Services)
+        {
+            Services.AddScoped<DefaultProvider>();
+        }
+    }
+}
