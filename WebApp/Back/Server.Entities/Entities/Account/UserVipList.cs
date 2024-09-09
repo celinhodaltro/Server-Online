b61e9f3dda145ce30342ecc55a5907217de22f2b
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-09-09 10:48:26 -0300

    Adicionar entidade de UserVipList e corrigir erros

---
+++ WebApp/Back/Server.Entities/Entities/Account/UserVipList.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Entities;
+public sealed class UserVipList
+{
+    public int AccountId { get; set; }
+    public int PlayerId { get; set; }
+    public string Description { get; set; }
+    public Player Player { get; set; }
+}
