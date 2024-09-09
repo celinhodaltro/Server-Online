3cf47125d1e924e5b2fdc5c94180f84377ec4e59
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-09-09 10:49:36 -0300

    Alterar using de sytem.entities para server.entities

--- WebApp/Back/Server.Entities/Entities/Default/DefaultDb.cs
+++ WebApp/Back/Server.Entities/Entities/Default/DefaultDb.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
-namespace System.Entities
+namespace Server.Entities
 {
     public class DefaultDb
     {
