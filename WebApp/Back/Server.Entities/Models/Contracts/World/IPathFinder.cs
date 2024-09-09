8276bcfa108122cad7f2d4a0a8803e9c328d364b
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-09-09 13:57:55 -0300

    Ajuste location.cs

--- WebApp/Back/Server.Entities/Models/Contracts/World/IPathFinder.cs
+++ WebApp/Back/Server.Entities/Models/Contracts/World/IPathFinder.cs
@@ -4,20 +4,19 @@ using Server.Entities.Models.Contracts.Creatures;
 using Server.Entities.Models.Contracts.World.Tiles;
 
 namespace Server.Entities.Models.Contracts.World;
-
 public interface IPathFinder
 {
     IMap Map { get; set; }
 
-    (bool Founded, Direction[] Directions) Find(ICreature creature, Location.Structs.Location target,
+    (bool Founded, Direction[] Directions) Find(ICreature creature, Location target,
         FindPathParams findPathParams,
         ITileEnterRule tileEnterRule);
 
-    (bool Founded, Direction[] Directions) Find(ICreature creature, Location.Structs.Location target,
+    (bool Founded, Direction[] Directions) Find(ICreature creature, Location target,
         ITileEnterRule tileEnterRule);
 
     Direction FindRandomStep(ICreature creature, ITileEnterRule rule);
 
-    Direction FindRandomStep(ICreature creature, ITileEnterRule rule, Location.Structs.Location origin,
+    Direction FindRandomStep(ICreature creature, ITileEnterRule rule, Location origin,
         int maxStepsFromOrigin = 1);
 }
