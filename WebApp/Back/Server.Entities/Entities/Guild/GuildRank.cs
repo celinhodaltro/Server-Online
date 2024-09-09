34622d010b4d7050a8edee6318e2710f123115c2
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-09-09 14:29:03 -0300

    Adicionar nullabe em GuildRank

--- WebApp/Back/Server.Entities/Entities/Guild/GuildRank.cs
+++ WebApp/Back/Server.Entities/Entities/Guild/GuildRank.cs
@@ -3,7 +3,7 @@
 public sealed class GuildRank : DefaultDb
 {
     public int GuildId { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int Level { get; set; }
-    public Guild Guild { get; set; }
+    public Guild? Guild { get; set; }
 }
