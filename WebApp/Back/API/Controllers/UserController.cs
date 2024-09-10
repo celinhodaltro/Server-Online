e23aa846af8cb145387590e156574641860a5b2c
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-09-10 14:24:44 -0300

    Mover função de tratamento de dados para BusinessRulles

--- WebApp/Back/API/Controllers/UserController.cs
+++ WebApp/Back/API/Controllers/UserController.cs
@@ -53,10 +53,8 @@ namespace API.Controllers
             try
             {
                 var result = await signManager.PasswordSignInAsync(userInfo?.Email, userInfo?.Password, isPersistent: false, lockoutOnFailure: false);
-
-
                 if (result.Succeeded)
-                    return BuildToken(userInfo);
+                    return userBusinessRules.BuildToken(userInfo);
                 else
                     return BadRequest("User or Password Invalid!");
 
@@ -69,35 +67,7 @@ namespace API.Controllers
 
         }
 
-        private UserToken BuildToken(User userinfo)
-        {
-            Claim[] claims = new[]
-            {
-                new Claim(JwtRegisteredClaimNames.UniqueName, userinfo.Email),
-                new Claim("AppMain", "Teste.com"),
-                new Claim(JwtRegisteredClaimNames.Aud, configuration["Jwt:Audience"]),
-                new Claim(JwtRegisteredClaimNames.Iss, configuration["Jwt:Issuer"]),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-            };
 
-            DateTime expiration = DateTime.UtcNow.AddHours(2);
-
-            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:key"]));
-            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            JwtSecurityToken token = new JwtSecurityToken(
-                issuer: null,
-                audience: null,
-                claims: claims,
-                expires: expiration,
-                signingCredentials: creds);
-
-            return new UserToken()
-            {
-                Token = new JwtSecurityTokenHandler().WriteToken(token),
-                Expiration = expiration
-            };
-        }
 
     }
 }
--- WebApp/Back/Server.BusinessRules/Account/UserBusinessRules.cs
+++ WebApp/Back/Server.BusinessRules/Account/UserBusinessRules.cs
@@ -1,6 +1,11 @@
 ﻿using Server.Entities;
 using System.Provider;
 using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Configuration;
 
 namespace Server.BusinessRules;
 
@@ -9,10 +14,15 @@ public class UserBusinessRules
 
     public DefaultProvider DefaultProvider { get; set; }
     public SignInManager<ApplicationUser> SignManager { get; set; }
-    public UserBusinessRules(DefaultProvider defaultProvider, SignInManager<ApplicationUser> signManager)
+    public LogBusinessRules LogBusinessRules { get; set; }
+    public IConfiguration Configuration { get; set; }
+
+    public UserBusinessRules(DefaultProvider defaultProvider, SignInManager<ApplicationUser> signManager, LogBusinessRules logBusinessRules, IConfiguration configuration)
     {
         DefaultProvider = defaultProvider;
         SignManager = signManager;
+        LogBusinessRules = logBusinessRules;
+        Configuration = configuration;
     }
 
     public async Task CreateUserInfo(int UserId)
@@ -33,13 +43,50 @@ public class UserBusinessRules
 
     public async Task<User?> GetUser(string? name, string? password)
     {
+        try
+        {
+            if (String.IsNullOrEmpty(name) || String.IsNullOrEmpty(password))
+                throw new Exception("Name or Password is empty");
 
-        if (String.IsNullOrEmpty(name) || String.IsNullOrEmpty(password))
-            throw new Exception("Name or Password is empty");
+            var Users = await DefaultProvider.GetAllAsync<User>();
+            return Users.FirstOrDefault(x => x.Email.Equals(name) && x.Password.Equals(password));
 
-        var Users = await DefaultProvider.GetAllAsync<User>();
-        return Users.FirstOrDefault(x => x.Email.Equals(name) && x.Password.Equals(password));
+        }
+        catch (Exception ex)
+        {
+            await LogBusinessRules.CreateLog(new LogTrack { Level = LogLevelEnum.Error, Details = ex.ToString(), Message = ex.Message });
+            throw;
+        }
+    }
 
+    public UserToken BuildToken(User userinfo)
+    {
+        Claim[] claims = new[]
+        {
+                new Claim(JwtRegisteredClaimNames.UniqueName, userinfo.Email),
+                new Claim("AppMain", "Teste.com"),
+                new Claim(JwtRegisteredClaimNames.Aud, Configuration["Jwt:Audience"]),
+                new Claim(JwtRegisteredClaimNames.Iss, Configuration["Jwt:Issuer"]),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+        DateTime expiration = DateTime.UtcNow.AddHours(2);
+
+        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:key"]));
+        SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        JwtSecurityToken token = new JwtSecurityToken(
+            issuer: null,
+            audience: null,
+            claims: claims,
+            expires: expiration,
+            signingCredentials: creds);
+
+        return new UserToken()
+        {
+            Token = new JwtSecurityTokenHandler().WriteToken(token),
+            Expiration = expiration
+        };
     }
 
 }
