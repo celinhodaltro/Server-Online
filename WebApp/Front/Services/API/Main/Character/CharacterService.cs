a8b4d5147f58c4fa8eff629c34e61001658e86f8
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-09-12 10:05:40 -0300

    Criar Character Service

---
+++ WebApp/Front/Services/API/Main/Character/CharacterService.cs
@@ -0,0 +1,72 @@
+﻿using Blazored.LocalStorage;
+using Server.Entities;
+using System.Text;
+using System.Text.Json;
+
+
+namespace WebApp.Services.API.Main
+{
+    public class CharacterService(IHttpClientFactory httpClientFactory)
+    {
+
+        public async Task<Player?> Create(Player? Character)
+        {
+            try
+            {
+                var httpClient = httpClientFactory.CreateClient("API");
+                var characterAsJson = JsonSerializer.Serialize(Character);
+                var requestContent = new StringContent(characterAsJson, Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync("api/Player/Create", requestContent);
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception(responseContent);
+
+               Character = JsonSerializer.Deserialize<Player>(responseContent,
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+
+                return Character;
+
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        public async Task<List<Player>> Create(int UserId)
+        {
+            try
+            {
+
+                var httpClient = httpClientFactory.CreateClient("API");
+                var UserIdAsJson = JsonSerializer.Serialize(UserId);
+                var requestContent = new StringContent(UserIdAsJson, Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync("api/Player/Create", requestContent);
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                    throw new Exception(responseContent);
+
+                var Characters = JsonSerializer.Deserialize<List<Player>>(responseContent,
+                     new JsonSerializerOptions
+                     {
+                         PropertyNameCaseInsensitive = true
+                     });
+
+                return Characters;
+
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+    }
+}
