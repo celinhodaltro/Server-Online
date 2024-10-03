2ea87036a9826150d3df6dce1cd5f0ef45ee3047
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-10-03 09:03:06 -0300

    Mudar referencia de IConnection

--- Game/src/ApplicationServer/Server.Contracts/Contracts/Network/IConnection.cs
+++ Game/src/ApplicationServer/Server.Contracts/Contracts/Network/IConnection.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Game.Common.Contracts.Creatures;
+using Server.Entities.Common.Contracts.Creatures;
 
 namespace Server.Common.Contracts.Network;
 
