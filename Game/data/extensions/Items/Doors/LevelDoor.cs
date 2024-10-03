9c66f856da8973d3c0374b8b2248e60636cae736
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-10-03 15:07:07 -0300

    Adicionar diretiva no Extension -> Door

--- Game/data/extensions/Items/Doors/LevelDoor.cs
+++ Game/data/extensions/Items/Doors/LevelDoor.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using Game.Common.Contracts.Creatures;
-using Game.Common.Contracts.Inspection;
-using Game.Common.Contracts.Items;
-using Game.Common.Item;
-using Game.Common.Location;
-using Game.Common.Location.Structs;
-using Game.Common.Services;
+using Server.Entities.Common.Contracts.Creatures;
+using Server.Entities.Common.Contracts.Inspection;
+using Server.Entities.Common.Contracts.Items;
+using Server.Entities.Common.Item;
+using Server.Entities.Common.Location;
+using Server.Entities.Common.Location.Structs;
+using Server.Entities.Common.Services;
 
 namespace Extensions.Items.Doors;
 
